6fc31e043a59c57437d3d6943ea8fdbff94b7a9c
Rik Faith <faith@alephnull.com>
2001-02-01 20:51:44 +0000

    Add DRM_MAJOR

--- shared/drm.h
+++ shared/drm.h
@@ -52,6 +52,7 @@
 #define DRM_DEV_GID	 0
 
 
+#define DRM_MAJOR       226
 #define DRM_NAME	"drm"	  /* Name in kernel, /dev, and /proc	    */
 #define DRM_MIN_ORDER	5	  /* At least 2^5 bytes = 32 bytes	    */
 #define DRM_MAX_ORDER	22	  /* Up to 2^22 bytes = 4MB		    */
