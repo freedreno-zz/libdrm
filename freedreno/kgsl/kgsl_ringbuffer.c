1baf03ac6e77049d9c8be1e3d5164283ce82c9db
Rob Clark <robclark@freedesktop.org>
2016-06-20 14:06:24 -0400

    WIP: freedreno: support growable cmdstream buffers
    The issue that userspace needed to solve is that there is ~two orders of
    magnitude size difference in cmdstream buffers (both for gmem commands
    and for draw commands), and that the previous practice of allocating
    worst-case sizes is quite wasteful.  Previously a submit would be
    constructed (for example) like:
    
      CMD  TARGET  DESCRIPTION
       g0    N     gmem/tiling commands
       b0    Y     binning commands
       d0    Y     draw commands
    
    Which, after the one non-IB-target cmd buffer is inserted into the
    kernel controlled ringbuffer, looks like (not to scale):
    
             b0:           d0:
            +-----+       +-----+
       IB1  | ... |       | ... |
            +-----+       +-----+
             ^             ^
             |             |
             +-----+       +-+---------+
             g0:   |         |         |
            +----+----+----+----+----+----+----
       IB0  | .. | IB | .. | IB | .. | IB | ...
            +----+----+----+----+----+----+----
             ^              tile0     tile1
             |
             +-----------+
      userspace          |
      ~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      kernel             |
                   ----+----+----
       ringbuffer  ... | IB | ...
                   ----+----+----
    
    Now, multiple physical cmdstream buffers per fd_ringbuffer are supported,
    so this becomes:
    
      CMD  TARGET  DESCRIPTION
       g0    N
       ...   N     gmem/tiling commands
       gN    N
       b0    Y
       ...   Y     binning commands
       bN    Y
       d0    Y
       ...   Y     draw commands
       dN    Y
    
    Which, after the non-IB-target cmd buffers (g0..gN) are inserted into
    the kernel controlled ringbuffer, looks like:
    
                 b0:      b1            d0:      d1
                +-----+  +-----+        +-----+  +-----+
           IB1  | ... |  | ... | ...    | ... |  | ... | ...
                +-----+  +-----+        +-----+  +-----+
                 ^        ^              ^        ^
                 |        |              |        |
                 |        +-+            |  +-----+------+
                 +-----+    |            |  |            |
                       |    |         +--+----------+    |
                 g0:   |    |         |     |       |    |
                +----+----+----+----+----+----+---+----+----+----
           IB0  | .. | IB | IB | .. | IB | IB |.. | IB | IB |...
                +----+----+----+----+----+----+---+----+----+----
                 ^                   tile0         tile1
                 | to b0  to b1
                 |   |      |          to|d0    to|d1
                 |   |      +----+       |      +-+-----------+
                 |   |           |       |      |             |
                 |   +------+    |       +-+-------------+    |
                 |    g1:   |    |         |    |        |    |
                 |   +----+----+----+----+----+----+---+----+----+----
           IB0   |   | .. | IB | IB | .. | IB | IB |.. | IB | IB |...
                 |   +----+----+----+----+----+----+---+----+----+----
                 |    ^                   tileX         tileY
                 |    |
                 |    +-----------+
                 +-----------+    |
          userspace          |    |
          ~~~~~~~~~~~~~~~~~~~|~~~~|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
          kernel             |    |
                       ----+----+----+----
           ringbuffer  ... | IB | IB | ...
                       ----+----+----+----

--- freedreno/kgsl/kgsl_ringbuffer.c
+++ freedreno/kgsl/kgsl_ringbuffer.c
@@ -174,11 +174,13 @@ static void kgsl_ringbuffer_emit_reloc(struct fd_ringbuffer *ring,
 }
 
 static void kgsl_ringbuffer_emit_reloc_ring(struct fd_ringbuffer *ring,
-		struct fd_ringbuffer *target,
+		struct fd_ringbuffer *target, uint32_t cmd_idx,
 		uint32_t submit_offset, uint32_t size)
 {
 	struct kgsl_ringbuffer *target_ring = to_kgsl_ringbuffer(target);
+	assert(cmd_idx == 0);
 	(*ring->cur++) = target_ring->bo->gpuaddr + submit_offset;
+	return size;
 }
 
 static void kgsl_ringbuffer_destroy(struct fd_ringbuffer *ring)
@@ -213,6 +215,7 @@ drm_private struct fd_ringbuffer * kgsl_ringbuffer_new(struct fd_pipe *pipe,
 
 	ring = &kgsl_ring->base;
 	ring->funcs = &funcs;
+	ring->size = size;
 
 	kgsl_ring->bo = kgsl_rb_bo_new(to_kgsl_pipe(pipe), size);
 	if (!kgsl_ring->bo) {
